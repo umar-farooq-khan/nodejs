31680ef9e11d0a3cb13e3b854f7e838a47b0df20
Umar Farooq khan <umaR.farooq407@gmail.com>
2023-08-22 12:19:11 +0200

    oij

--- index.js
+++ index.js
@@ -1,37 +1,14 @@
-const http = require('http');
+const express = require('express');
+const cors = require('cors');
 const PORT = 3000;
-const cors = require("cors");
-app.use(cors());
-const server = http.createServer((req, res) => {
-    // Manually setting the CORS headers
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allowed methods
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allowed headers
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World!');
+const app = express();
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
 });
 
-
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}/`);
 });
-
-/*
-app.use(cors());
-
-const http = require('http');
-const PORT = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hellokoko World!');
-});
-
-server.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
-});
-*/
