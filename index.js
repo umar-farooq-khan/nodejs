978d5f931d29582faf4d32aedfd1e2707e730baa
Umar Farooq khan <umaR.farooq407@gmail.com>
2023-08-22 12:09:44 +0200

    oi

--- index.js
+++ index.js
@@ -2,6 +2,27 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
+    // Manually setting the CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow all origins
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Allowed methods
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type'); // Allowed headers
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World!');
+});
+
+server.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}/`);
+});
+
+/*
+app.use(cors());
+
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hellokoko World!');
@@ -10,3 +31,4 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}/`);
 });
+*/
